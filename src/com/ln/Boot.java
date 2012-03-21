c6ff0fa1be484aef8ec264a6974105a37ab32161
Jiiks GG <jiiks@windowslive.com>
2012-03-21 15:46:10 +0200

    switch > if

--- src/com/ln/Boot.java
+++ src/com/ln/Boot.java
@@ -80,36 +80,92 @@ public class Boot extends JFrame {
 			
 		}
 		try{
-			if (Configuration.theme == 0){UIManager.setLookAndFeel(new SubstanceAutumnLookAndFeel());Configuration.theme = 0;}
-			if (Configuration.theme == 1){UIManager.setLookAndFeel(new SubstanceBusinessBlackSteelLookAndFeel());Configuration.theme = 1;}
-			if (Configuration.theme == 2){UIManager.setLookAndFeel(new SubstanceBusinessBlueSteelLookAndFeel());Configuration.theme = 2;}
-			if (Configuration.theme == 3){UIManager.setLookAndFeel(new SubstanceCremeCoffeeLookAndFeel());Configuration.theme = 3;}
-			if (Configuration.theme == 4){UIManager.setLookAndFeel(new SubstanceChallengerDeepLookAndFeel());Configuration.theme = 4;}
-			if (Configuration.theme == 5){UIManager.setLookAndFeel(new SubstanceDustLookAndFeel());Configuration.theme = 5;}
-			if (Configuration.theme == 6){UIManager.setLookAndFeel(new SubstanceDustCoffeeLookAndFeel());Configuration.theme = 6;}
-			if (Configuration.theme == 7){UIManager.setLookAndFeel(new SubstanceEmeraldDuskLookAndFeel());Configuration.theme = 7;}
-			if (Configuration.theme == 8){UIManager.setLookAndFeel(new SubstanceGeminiLookAndFeel());Configuration.theme = 8;}
-			if (Configuration.theme == 9){UIManager.setLookAndFeel(new SubstanceGraphiteLookAndFeel());Configuration.theme = 9;}
-			if (Configuration.theme == 10){UIManager.setLookAndFeel(new SubstanceGraphiteAquaLookAndFeel());Configuration.theme = 10;}
-			if (Configuration.theme == 11){UIManager.setLookAndFeel(new SubstanceGraphiteGlassLookAndFeel());Configuration.theme = 11;}
-			if (Configuration.theme == 12){UIManager.setLookAndFeel(new SubstanceMagellanLookAndFeel());Configuration.theme = 12;}
-			if (Configuration.theme == 13){UIManager.setLookAndFeel(new SubstanceMarinerLookAndFeel());Configuration.theme = 13;}
-			if (Configuration.theme == 14){UIManager.setLookAndFeel(new SubstanceMistAquaLookAndFeel());Configuration.theme = 14;}
-			if (Configuration.theme == 15){UIManager.setLookAndFeel(new SubstanceMistSilverLookAndFeel());Configuration.theme = 15;}
-			if (Configuration.theme == 16){UIManager.setLookAndFeel(new SubstanceModerateLookAndFeel());Configuration.theme = 16;}
-			if (Configuration.theme == 17){UIManager.setLookAndFeel(new SubstanceNebulaLookAndFeel());Configuration.theme = 17;}
-			if (Configuration.theme == 18){UIManager.setLookAndFeel(new SubstanceNebulaBrickWallLookAndFeel());Configuration.theme = 18;}
-			if (Configuration.theme == 19){UIManager.setLookAndFeel(new SubstanceOfficeBlack2007LookAndFeel());Configuration.theme = 19;}
-			if (Configuration.theme == 20){UIManager.setLookAndFeel(new SubstanceOfficeBlue2007LookAndFeel());Configuration.theme = 20;}
-			if (Configuration.theme == 21){UIManager.setLookAndFeel(new SubstanceOfficeSilver2007LookAndFeel());Configuration.theme = 21;}
-			if (Configuration.theme == 22){UIManager.setLookAndFeel(new SubstanceRavenLookAndFeel());Configuration.theme = 22;}
-			if (Configuration.theme == 23){UIManager.setLookAndFeel(new SubstanceSaharaLookAndFeel());Configuration.theme = 23;}				
-			if (Configuration.theme == 24){UIManager.setLookAndFeel(new SubstanceBusinessLookAndFeel());Configuration.theme = 24;}
-			if (Configuration.theme == 25){UIManager.setLookAndFeel(new SubstanceTwilightLookAndFeel());Configuration.theme = 25;}
-			setDefaultLookAndFeelDecorated(true);
+		switch(Configuration.theme){	
+		case 0:
+			UIManager.setLookAndFeel(new SubstanceAutumnLookAndFeel());Configuration.theme = 0;
+				break;
+		case 1:
+			UIManager.setLookAndFeel(new SubstanceBusinessBlackSteelLookAndFeel());Configuration.theme = 1;
+				break;
+		case 2:
+			UIManager.setLookAndFeel(new SubstanceBusinessBlueSteelLookAndFeel());Configuration.theme = 2;
+				break;
+		case 3:
+			UIManager.setLookAndFeel(new SubstanceCremeCoffeeLookAndFeel());Configuration.theme = 3;
+				break;
+		case 4:
+			UIManager.setLookAndFeel(new SubstanceChallengerDeepLookAndFeel());Configuration.theme = 4;
+				break;
+		case 5:
+			UIManager.setLookAndFeel(new SubstanceDustLookAndFeel());Configuration.theme = 5;
+				break;
+		case 6:
+			UIManager.setLookAndFeel(new SubstanceDustCoffeeLookAndFeel());Configuration.theme = 6;
+				break;
+		case 7:
+			UIManager.setLookAndFeel(new SubstanceEmeraldDuskLookAndFeel());Configuration.theme = 7;
+				break;
+		case 8:
+			UIManager.setLookAndFeel(new SubstanceGeminiLookAndFeel());Configuration.theme = 8;
+				break;
+		case 9:
+			UIManager.setLookAndFeel(new SubstanceGraphiteLookAndFeel());Configuration.theme = 9;
+				break;
+		case 10:
+			UIManager.setLookAndFeel(new SubstanceGraphiteAquaLookAndFeel());Configuration.theme = 10;
+				break;
+		case 11:
+			UIManager.setLookAndFeel(new SubstanceGraphiteGlassLookAndFeel());Configuration.theme = 11;
+				break;
+		case 12:
+			UIManager.setLookAndFeel(new SubstanceMagellanLookAndFeel());Configuration.theme = 12;
+				break;
+		case 13:
+			UIManager.setLookAndFeel(new SubstanceMarinerLookAndFeel());Configuration.theme = 13;
+				break;
+		case 14:	
+			UIManager.setLookAndFeel(new SubstanceMistAquaLookAndFeel());Configuration.theme = 14;
+				break;
+		case 15:	
+			UIManager.setLookAndFeel(new SubstanceMistSilverLookAndFeel());Configuration.theme = 15;
+				break;
+		case 16:	
+			UIManager.setLookAndFeel(new SubstanceModerateLookAndFeel());Configuration.theme = 16;
+				break;
+		case 17:	
+			UIManager.setLookAndFeel(new SubstanceNebulaLookAndFeel());Configuration.theme = 17;
+				break;
+		case 18:
+			UIManager.setLookAndFeel(new SubstanceNebulaBrickWallLookAndFeel());Configuration.theme = 18;
+				break;
+		case 19:
+			UIManager.setLookAndFeel(new SubstanceOfficeBlack2007LookAndFeel());Configuration.theme = 19;
+				break;
+		case 20:
+			UIManager.setLookAndFeel(new SubstanceOfficeBlue2007LookAndFeel());Configuration.theme = 20;
+				break;
+		case 21:
+			UIManager.setLookAndFeel(new SubstanceOfficeSilver2007LookAndFeel());Configuration.theme = 21;
+				break;
+		case 22:
+			UIManager.setLookAndFeel(new SubstanceRavenLookAndFeel());Configuration.theme = 22;
+				break;
+		case 23:
+			UIManager.setLookAndFeel(new SubstanceSaharaLookAndFeel());Configuration.theme = 23;				
+				break;
+		case 24:
+			UIManager.setLookAndFeel(new SubstanceBusinessLookAndFeel());Configuration.theme = 24;
+				break;
+		case 25:
+			UIManager.setLookAndFeel(new SubstanceTwilightLookAndFeel());Configuration.theme = 25;
+				break;
+		default:
+			UIManager.setLookAndFeel(new SubstanceGraphiteGlassLookAndFeel());Configuration.theme = 11;
+				break;
 		}
-		catch(Exception e){			
-		}
+		}catch (Exception e){}		
+		setDefaultLookAndFeelDecorated(true);
+
 		Newsreader.Main();
 		@SuppressWarnings("rawtypes")
 		Main frame = new Main();
